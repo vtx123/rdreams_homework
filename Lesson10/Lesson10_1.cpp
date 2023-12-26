d30c174d8131e32ca27240221aefe1c210fb3796
vtx123 <vitaly.galavinsky@gmail.com>
2023-12-26 14:09:58 +0200

    fixes by PR comments

--- Lesson10/Lesson10_1.cpp
+++ Lesson10/Lesson10_1.cpp
@@ -2,13 +2,12 @@
 #include <iostream>
 
 void swap(int& a, int& b) {
-    int tmp = 0;
-    tmp = a;
+    int tmp = a;
     a = b;
     b = tmp;
 }
 
-void swap(int* a, int* b){
+void swap(int* a, int* b) {
     int tmp = 0;
     tmp = *a;
     *a = *b;
@@ -19,9 +18,9 @@ int main() {
     int a = 0;
     int b = 0;
 
-    std::cout << "Please, enter a first number: " <<std::endl;
+    std::cout << "Please, enter a first number: " << std::endl;
     std::cin >> a;
-    std::cout << "Please, enter a second number: " <<std::endl;
+    std::cout << "Please, enter a second number: " << std::endl;
     std::cin >> b;
 
     std::cout << "Output 1 swap ref:" << std::endl;
@@ -29,7 +28,7 @@ int main() {
     std::cout << "before swap ref: " << &a << " " << &b << std::endl;
     swap(a, b);
     std::cout << "after swap ref: " << a << " " << b << std::endl;
-    std::cout << "after swap ref: " << &a << " " << &b << std::endl<< std::endl;
+    std::cout << "after swap ref: " << &a << " " << &b << std::endl << std::endl;
 
     std::cout << "Output 2 swap ptr:" << std::endl;
     std::cout << "before swap ptr: " << a << " " << b << std::endl;
--- Lesson10/Lesson10_2.cpp
+++ Lesson10/Lesson10_2.cpp
@@ -2,11 +2,11 @@
 #include <iostream>
 
 bool calculateSum(const double* arr, int arrSize, double& sum) {
-    if(arrSize <= 0){
+    if (arrSize <= 0) {
         return false;
     }
 
-    for(int i = 0; i < arrSize; i++){
+    for (int i = 0; i < arrSize; i++) {
         sum += arr[i];
     }
     return true;
@@ -14,16 +14,20 @@ bool calculateSum(const double* arr, int arrSize, double& sum) {
 
 int main() {
     int arraySize = 0;
-    double sum = 0;
+    double sum = 0.0;
 
-    std::cout << "Please, enter an array size: " <<std::endl;
+    std::cout << "Please, enter an array size: " << std::endl;
     std::cin >> arraySize;
 
-    double inputArray[arraySize];
-    for(int i = 0; i < arraySize; i++){
+    double *inputArray = new double[arraySize];
+    for (int i = 0; i < arraySize; i++) {
+        std::cout << "Please enter a " << i + 1 << " number: " << std::endl;
         std::cin >> inputArray[i];
     }
 
-    std::cout << "Output: " << std::boolalpha <<calculateSum(inputArray, arraySize, sum) << " Sum: " << sum << std::endl;
+    std::cout << std::endl << std::endl;
+    std::cout << "Output: " << std::boolalpha << calculateSum(inputArray, arraySize, sum) << " Sum: " << sum << std::endl;
+
+    delete[] inputArray;
     return 0;
 }
