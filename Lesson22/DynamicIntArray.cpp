aec35bdf8c69a0b67d13463702d78bda255aa32c
vtx123 <vitaly.galavinsky@gmail.com>
2024-02-06 17:22:43 +0200

    code from lesson 19

---
+++ Lesson22/DynamicIntArray.cpp
@@ -0,0 +1,97 @@
+#include "DynamicIntArray.h"
+#include <iostream>
+
+// Default c-tor
+DynamicIntArray::DynamicIntArray() :m_size(0), m_position(0) {
+    m_elements = new int[m_size];
+    std::cout << "Defalult c-tor" << std::endl;
+}
+
+// C-tor with size
+DynamicIntArray::DynamicIntArray(std::size_t size) : m_size(size), m_position(0) {
+    m_elements = new int[m_size];
+    std::cout << "C-tor with size" << std::endl;
+}
+
+// Copy c-tor
+DynamicIntArray::DynamicIntArray(const DynamicIntArray& other) {
+    m_size = other.m_size;
+    m_position = other.m_position;
+    m_elements = new int[other.m_size];
+
+    std::copy(other.m_elements, other.m_elements + other.m_size, m_elements);
+    std::cout << "Copy c-tor" << std::endl;
+}
+
+// Free allocated memory
+DynamicIntArray::~DynamicIntArray() {
+    std::cout << "D-tor" << std::endl;
+    delete[] m_elements;
+}
+
+DynamicIntArray& DynamicIntArray::operator=(const DynamicIntArray& other) {
+    DynamicIntArray tmp(other);
+    delete[] m_elements;
+    return tmp;
+}
+
+int& DynamicIntArray::operator[](std::size_t index) {
+    if (index >= m_size) {
+        index = m_size - 1;
+    }
+    return m_elements[index];
+}
+
+// Delete previous memory, create new inner array with updated size
+void DynamicIntArray::setSize(std::size_t newSize) {
+    if (m_size < newSize) {
+        int* tmp = new int[m_size];
+        std::copy(m_elements, m_elements + m_size, tmp);
+        m_position = 0;
+
+        delete[] m_elements;
+        m_elements = new int[newSize];
+        for (int i = 0; i < m_size; i++) {
+            m_elements[i] = tmp[i];
+            m_position++;
+        }
+        delete[] tmp;
+    }
+
+    m_size = newSize;
+}
+
+std::size_t DynamicIntArray::getSize() const {
+    return m_size;
+}
+
+// Clear internal memory, set size to 0
+void DynamicIntArray::clear() {
+    m_size = 0;
+    delete[] m_elements;
+}
+
+// Allocate new memory for new element
+// Copy old content to new inner array
+// insert element at the last index
+// update size
+void DynamicIntArray::push_back(int element) {
+    if (m_position + 1 >= m_size) {
+        int* tmp = new int[m_position + 1];
+        for (int i = 0; i < m_size; i++) {
+            tmp[i] = m_elements[i];
+        }
+        tmp[m_position] = element;
+        m_size = m_position + 1;
+        delete[] m_elements;
+        m_elements = new int[m_size];
+        for (int i = 0; i < m_size; i++) {
+            m_elements[i] = tmp[i];
+        }
+        delete[] tmp;
+    }
+    else {
+        m_elements[m_position] = element;
+    }
+    m_position++;
+}
---
+++ Lesson22/Lesson22.cpp
@@ -0,0 +1,51 @@
+// Lesson 22
+
+#include <iostream>
+#include "DynamicIntArray.h"
+
+int main()
+{
+    DynamicIntArray arr1;
+    DynamicIntArray arr2 = DynamicIntArray(10);
+
+    const int arr1NewSize = 5;
+    arr1.setSize(arr1NewSize);
+    const int arr1Size = arr1.getSize();
+    for (int i = 0; i < arr1Size; i++) {
+        arr1.push_back(i * 10);
+        std::cout << "[" << arr1[i] << "] ";
+    }
+    std::cout << std::endl << std::endl;
+
+    const int arr2Size = arr2.getSize();
+    for (int i = 0; i < arr2Size; i++) {
+        arr2.push_back(i);
+        std::cout << "[" << arr2[i] << "] ";
+    }
+    std::cout << std::endl << std::endl;
+
+    const int arr2NewSize = 15;
+    arr2.setSize(arr2NewSize);
+    for (int i = 0; i < arr2NewSize; i++) {
+        arr2.push_back(i + 10);
+        std::cout << "[" << arr2[i] << "] ";
+    }
+    std::cout << std::endl << std::endl;
+
+    DynamicIntArray arr3(arr1);
+    const int arr4Size = arr3.getSize();
+    for (int i = 0; i < arr4Size; i++) {
+        std::cout << "[" << arr3[i] << "] ";
+    }
+    std::cout << std::endl << std::endl;
+
+    arr3.push_back(100);
+    arr3.push_back(300);
+
+    for (int i = 0; i < arr3.getSize(); i++) {
+        std::cout << "[" << arr3[i] << "] ";
+    }
+    std::cout << std::endl << std::endl;
+
+    return 0;
+}
