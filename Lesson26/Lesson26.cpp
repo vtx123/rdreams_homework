fd4d07670fdd9593f866c454f5d011054a49245e
vtx123 <vitaly.galavinsky@gmail.com>
2024-02-15 20:13:32 +0200

    Lesson 26

--- Lesson26/Lesson26.cpp
+++ Lesson26/Lesson26.cpp
@@ -5,43 +5,35 @@
 
 int main()
 {
-    Vector2d testVec{ 1.2f, 5.6f };
-    std::cout << testVec << std::endl;
+    Vector2d v2d_1{ 1.0f, 1.0f, 3.0f, 4.0f };
 
-    Vector2d* v2d0 = new Vector2d();
-    std::cout << *v2d0 << std::endl;
+    Vector2d v2d_2{ 1.0f, 2.0f};
+    Vector2d v2d_3{ 3.0f, 4.0f };
+    std::cout << "Dot Product = " << v2d_2.dotProduct(v2d_3) << std::endl;
+    std::cout << "Inversion = " << v2d_2 << " transform to " << v2d_2.negate() << std::endl << std::endl;
 
-    Vector2d* v2d1 = new Vector2d(1.1f, 2.2f);
-    Vector2d* v2d2 = new Vector2d(5.1f, 6.2f);
-    Vector2d v2d3 = Vector2d(5.1f, 6.2f);
-    Vector2d v2d4 = Vector2d(0.1f, 0.2f);
+    Vector2d v1{ 1.0f, 2.0f };
+    Vector2d v2{ 2.0f, -1.0f };
+    Vector2d v3{ -1.0f, -2.0f };
+    Vector2d v4{ 2.0f, 3.0f };
 
-    v2d4 = v2d4 - *v2d1;
-    std::cout << v2d4 << std::endl;
+    std::cout << "--- Relative State Test --- " << std::endl;
+    std::cout << relativeToStr(v1.getRelativeState(v1)) << std::endl;
+    std::cout << relativeToStr(v1.getRelativeState(v2)) << std::endl;
+    std::cout << relativeToStr(v1.getRelativeState(v3)) << std::endl;
+    std::cout << relativeToStr(v1.getRelativeState(v4)) << std::endl << std::endl;
 
-    v2d3 = v2d3 - *v2d2;
-    std::cout << v2d3 << std::endl;
-    std::cout << v2d3() << std::endl;
-    std::cout << v2d3[0] << std::endl;
-    std::cout << v2d3[1] << std::endl;
+    std::cout << relativeToStr(v1.getRelativeState2(v1)) << std::endl;
+    std::cout << relativeToStr(v1.getRelativeState2(v2)) << std::endl;
+    std::cout << relativeToStr(v1.getRelativeState2(v3)) << std::endl;
+    std::cout << relativeToStr(v1.getRelativeState2(v4)) << std::endl << std::endl;
 
-    std::cout << *v2d1 << std::endl;
-    v2d1 = v2d2;
-    std::cout << *v2d1 << std::endl;
+    std::cout << "Scale = " << v2d_3 << " transform to ";
+    v2d_3.scale(2.0f, 2.0f);
+    std::cout << v2d_3 << std::endl;
 
-    Vector2d v1 = Vector2d(3.3f, 4.4f);
-    Vector2d v2 = Vector2d(1.1f, 2.2f);
-    std::cout << v1 << std::endl;
-    std::cout << v2 << std::endl;
+    Vector3d vec3_1 = Vector3d(1.0f, 2.0f, 3.0f);
+    Vector3d vec3_2 = Vector3d(4.0f, 5.0f, 6.0f);
 
-    Vector2d dif = v1 - v2;
-    Vector2d sum = v1 + v2;
-    std::cout << dif << std::endl;
-    std::cout << sum << std::endl;
-
-    std::cout << Vector2d::instanceCount() << std::endl;
-
-    std::cin >> sum;
-    std::cout << sum << std::endl;
-
+    std::cout << "Cross Product = " << vec3_1.crossProduct(vec3_2) << std::endl;
 }
--- Lesson26/Vector2d.cpp
+++ Lesson26/Vector2d.cpp
@@ -9,6 +9,10 @@ Vector2d::Vector2d(float x, float y) : _x(x), _y(y) {
     _icount++;
 }
 
+Vector2d::Vector2d(float x0, float y0, float x1, float y1) : _x(x1 - x0), _y(y1 - y0) {
+    _icount++;
+}
+
 Vector2d::Vector2d(const Vector2d& copy) :_x(copy._x), _y(copy._y) {
     _icount++;
 }
@@ -74,4 +78,77 @@ Vector2d operator-(const Vector2d& leftVector, const Vector2d& rightVector) {
     float x = leftVector._x - rightVector._x;
     float y = leftVector._y - rightVector._y;
     return Vector2d(x, y);
+}
+
+float Vector2d::magnitude() const {
+    return static_cast<float>(sqrt(pow(_x, 2) + pow(_y, 2)));
+}
+
+float Vector2d::dotProduct(Vector2d& other) const 
+{
+    return _x * other._x + _y * other._y;
+}
+
+Vector2d Vector2d::negate() const 
+{
+    return Vector2d(_x * -1, _y * -1);
+}
+
+VectorRelativeState Vector2d::getRelativeState(const Vector2d& other) const 
+{
+    float dp = dotProduct(const_cast<Vector2d&>(other));
+
+    if (_x == other._x && _y == other._y && magnitude() == other.magnitude()) {
+        return VectorRelativeState::Identical;
+    }
+    if ( dp > 0) {
+        return VectorRelativeState::coDirected;
+    }
+    if (dp == 0) {
+        return VectorRelativeState::RightAngle;
+    }
+    if (dp < 0) {
+        return VectorRelativeState::OppositeDirected;
+    }
+}
+
+VectorRelativeState Vector2d::getRelativeState2(const Vector2d& other) const 
+{
+    float dot = _x * other._x + _y * other._y;
+    float det = _x * other._y - _y * other._x;
+    int angle = static_cast<int>(180 / 3.14 * atan2(det, dot));
+
+    std::cout << "A cos = " << angle << std::endl;
+    if (angle < 90 && angle > -90) {
+        return VectorRelativeState::AcuteAngle;
+    }
+    else {
+        return VectorRelativeState::ObtuseAngle;
+    }
+}
+
+void Vector2d::scale(float factorX, float factorY) 
+{
+    _x *= factorX;
+    _y *= factorY;
+}
+
+Vector3d::Vector3d() : _x(0), _y(0), _z(0)
+{
+}
+Vector3d::Vector3d(float x, float y, float z) : _x(x), _y(y), _z(z)
+{
+}
+
+std::ostream& operator << (std::ostream& out, const Vector3d& vec3d) {
+    out << "Output: {" << vec3d._x << "; " << vec3d._y << "; " << vec3d._z << "}";
+    return out;
+}
+
+Vector3d Vector3d::crossProduct(const Vector3d& other)
+{
+    float x = _y * other._z - _z * other._y;
+    float y = _z * other._x - _x * other._z;
+    float z = _x * other._y - _y * other._x;
+    return Vector3d(x, y, z);
 }
